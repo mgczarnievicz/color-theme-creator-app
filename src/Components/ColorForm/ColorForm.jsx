026565df1b3a0aab1955a8923123aaa1b18ab3d1
Gime Cz <mgczarnievicz@gmail.com>
2025-01-17 08:28:12 +0100

    Add ColorForm component

---
+++ src/Components/ColorForm/ColorForm.jsx
@@ -0,0 +1,30 @@
+// import React from 'react';
+import ColorInput from '../ColorInput/ColorInput';
+
+import './ColorForm.css';
+
+export default function ColorForm({ onAddColor }) {
+	function handleSubmit(e) {
+		e.preventDefault();
+
+		const dataForm = new FormData(e.target);
+		const data = Object.fromEntries(dataForm.entries());
+
+		console.log('Submit', data);
+		e.target.reset();
+		onAddColor(data);
+	}
+	return (
+		<form onSubmit={handleSubmit}>
+			<label htmlFor='role'>Role</label>
+			<input type='text' name='role' id='role' />
+			<label htmlFor='color'>Hexa</label>
+
+			<ColorInput name='hex' initColor={'#123456'} />
+			<label htmlFor='color'>Contract Text</label>
+
+			<ColorInput name='contrastText' initColor={'#148491'} />
+			<button type='submit'>Add</button>
+		</form>
+	);
+}
